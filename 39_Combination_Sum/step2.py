36dca6068891fd05bf7e079143a719cb388e4347
Ryoma Ishida <125534009+ryomappi@users.noreply.github.com>
2026-04-27 15:52:55 +0900

    ステップ2途中まで

---
+++ 39_Combination_Sum/step2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        def backtrack(start, current, remaining):
+            if remaining == 0:
+                result.append(current[:])
+                return
+            if remaining < 0:
+                return
+
+            for i in range(start, len(candidates)):
+                backtrack(i, current + [candidates[i]], remaining - candidates[i])
+
+        backtrack(0, [], target)
+        return result
